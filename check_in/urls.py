5f79b59f2bdcfec4c91cc0625960339739381ef9
Ryan Smick <ryanjsmick@gmail.com>
2017-03-01 16:37:55 -0500

    Added signin functionality Implemented functionality for signin via modal and redirection to new page. Still must implement usability features for modal and second html template for logged in users.

--- check_in/urls.py
+++ check_in/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^login$', views.user_login, name='user_login'),
 ]
--- check_in/views.py
+++ check_in/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'check_in/index.html')
+
+@require_POST
+def user_login(request):
+    username = request.POST['email']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return JsonResponse({'result': 'success', 'redirect_url': '/'})
+        
+    else:
+        # Return an 'invalid login' error message.
+        return JsonResponse({'result': 'error', 'message': 'Invalid username or password'})
