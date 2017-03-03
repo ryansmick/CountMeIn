17e53676061f9a72a0c86ae787702673fc39da47
Ryan Smick <ryanjsmick@gmail.com>
2017-03-02 22:24:35 -0500

    Added several files that slipped through the cracks

---
+++ CountMeIn/staticfiles/check_in/js/authed_base.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    //Function to obtain a cookie's value
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    
+    //Submit button handler for login button
+    $('#logoutBtn').click(function(){
+        $.ajax({
+            url: '/logout',
+            type: 'POST',
+            data: {
+                'csrfmiddlewaretoken': getCookie('csrftoken')
+            },
+            success: function(result, status, xhr){
+                    //Redirect to specified page
+                    window.location.href = result.redirect_url;
+            }
+        });
+    });
+    
+});
