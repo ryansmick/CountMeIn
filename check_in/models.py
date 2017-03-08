005446e5807b3010eea6dce0c1e21de50d8ecb4d
Ryan Smick <ryanjsmick@gmail.com>
2017-03-07 23:01:35 -0500

    Created Group database table

--- check_in/models.py
+++ check_in/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=40)
+    visible = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+    admins = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return self.name
+    
